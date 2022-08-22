833cba9d8af8dde7ca74b614a7be9dccde482e09
cnhemiya <cnhemiya@qq.com>
2022-08-22 12:55:14 +0800

    更新 utils.py

--- mod/utils.py
+++ mod/utils.py
@@ -1,7 +1,43 @@
+import os
+import sys
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+import hashlib
+import shutil
 
 
 def setMenu(menu:QtWidgets.QMenu, text: str, triggered):
     """设置菜单"""
     action = menu.addAction(text)
     action.triggered.connect(triggered)
+
+def fileMD5(file_path: str):
+    """计算文件的MD5值"""
+    md5 = hashlib.md5()
+    with open(file_path, 'rb') as f:
+        md5.update(f.read())
+    return md5.hexdigest()
+
+def copyFile(from_path: str, to_path: str):
+    """复制文件"""
+    shutil.copyfile(from_path, to_path)
+    return os.path.exists(to_path)
+
+def removeFile(file_path: str):
+    """删除文件"""
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    return not os.path.exists(file_path)
+
+def fileExtension(file_path: str):
+    """获取文件的扩展名"""
+    return os.path.splitext(file_path)[1]
+
+def newFile(file_path: str):
+    """创建文件"""
+    if os.path.exists(file_path):
+        return False
+    else:
+        with open(file_path, 'w') as f:
+            pass
+        return True 
