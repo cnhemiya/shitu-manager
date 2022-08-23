6ccdefb6d0ab93b32233e57d82abd456d8ba34c2
cnhemiya <cnhemiya@qq.com>
2022-08-23 20:38:00 +0800

    增加 一键从其它图库导入数据 oneKeyImport

--- devel_doc/test.py
+++ devel_doc/test.py
@@ -7,13 +7,17 @@ import sys
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(__dir__, '../')))
 
-import hashlib
-import shutil
 import urllib.parse
+import mod.utils
+import mod.image_list_manager
 
-def main():
+def url_decode():
+    # url 转码
     ss = """image_list_path=image_list.txt&index_root_path=%2Fhome%2Fuser_home%2Ftest_lib&index_method=HNSW32&force=True"""
     print(urllib.parse.parse_qs(ss))
+
+def main():
+    url_decode()
 
 if __name__ == '__main__':
     main()
--- mod/utils.py
+++ mod/utils.py
@@ -1,9 +1,13 @@
 import os
 import sys
 
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(__dir__, '../')))
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 import hashlib
 import shutil
+import mod.image_list_manager
 
 
 def setMenu(menu:QtWidgets.QMenu, text: str, triggered):
@@ -32,6 +36,47 @@ def removeFile(file_path: str):
 def fileExtension(file_path: str):
     """获取文件的扩展名"""
     return os.path.splitext(file_path)[1]
+
+def copyImageToDir(self, from_image_path: str, to_dir_path: str):
+    """复制图像文件到目标目录"""
+    if not os.path.exists(from_image_path) and not os.path.exists(to_dir_path):
+        return None
+    md5 = fileMD5(from_image_path)
+    file_ext = fileExtension(from_image_path)
+    new_path = os.path.join(to_dir_path, md5 + file_ext)
+    copyFile(from_image_path, new_path)
+    return new_path
+
+def oneKeyImport(from_path: str, to_path: str):
+    """从其它图像库 from_path {image_list.txt} 导入到图像库 to_path {image_list.txt}"""
+    if not os.path.exists(from_path) and not os.path.exists(to_path):
+        return None
+    if from_path == to_path:
+        return None
+    from_mgr = mod.image_list_manager.ImageListManager(file_path=from_path)
+    to_mgr = mod.image_list_manager.ImageListManager(file_path=to_path)
+    count = 0
+    for classify in from_mgr.classifyList:
+        img_list = from_mgr.realPathList(classify)
+        to_mgr.addClassify(classify)
+        to_img_list = to_mgr.imageList(classify)
+        new_img_list = []
+        for img in img_list:
+            from_image_path = img
+            to_dir_path = os.path.join(to_mgr.dirName, "images")
+            md5 = fileMD5(from_image_path)
+            file_ext = fileExtension(from_image_path)
+            new_path = os.path.join(to_dir_path, md5 + file_ext)
+            if os.path.exists(new_path):
+                # 如果新文件 MD5 重复跳过后面的复制文件操作
+                continue
+            copyFile(from_image_path, new_path)
+            new_img_list.append("images/" + md5 + file_ext)
+            count += 1
+        to_img_list += new_img_list
+        to_mgr.resetImageList(classify, to_img_list)
+    to_mgr.writeFile()
+    return count
 
 def newFile(file_path: str):
     """创建文件"""
